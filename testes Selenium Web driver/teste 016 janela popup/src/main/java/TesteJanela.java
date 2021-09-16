3d2073111bb4954c65174952f7f5196957baee89
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2021-09-16 15:44:26 -0300

    teste 16 e 17
    janela pop up/ handle

---
+++ testes Selenium Web driver/teste 016 janela popup/src/main/java/TesteJanela.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Driver;
+
+// teste com iframe
+public class TesteJanela {
+    @Test
+    public void deveInteragirComFrame() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.switchTo().frame("frame1");
+        driver.findElement(By.id("frameButton")).click();
+        Alert alert = driver.switchTo().alert();
+        String msg = alert.getText();
+        Assert.assertEquals("Frame OK!", msg);
+        alert.accept();
+
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("elementosForm:nome")).sendKeys(msg);
+
+    }
+
+// teste janela
+    @Test
+    public void deveInteragircomJanela(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("buttonPopUpEasy")).click();
+        driver.switchTo().window("Popup");
+        driver.findElement(By.tagName("textarea")).sendKeys(("Deu certo?"));
+        driver.close();
+       // driver.switchTo().window("Popup");
+       // driver.findElement(By.tagName("textarea")).sendKeys("e agora?");
+
+    }
+}
