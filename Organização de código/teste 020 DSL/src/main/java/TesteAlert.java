30b099a5dd66356b6c0c3f2ec4a84be363aea8fe
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2021-09-24 17:27:25 -0300

    Dsl
    como limpar um alinha antes de  escrever o proximo elemento a testar

--- Organização de código/teste 020 DSL/src/main/java/TesteAlert.java
+++ Organização de código/teste 020 DSL/src/main/java/TesteAlert.java
@@ -2,70 +2,54 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-//Alert simple
 public class TesteAlert {
+	
+	private WebDriver driver;
+	private DSL dsl;
+	
+	@Before
+	public void inicializa(){
+		driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		dsl = new DSL(driver);
+	}
+	
+	@After
+	public void finaliza(){
+		driver.quit();
+	}
 
-    // criação de metodos para agilisar e reduzir os códigos de teste.
-    private WebDriver driver;
-
-    @Before
-    public void inicializa(){
-        driver = new FirefoxDriver();
-        driver.manage().window().setSize(new Dimension(1200, 765));
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-    }
-    @After
-    public  void ficnalizar(){
-        driver.quit();
-    }
-
-    // inicialização da suite de testes.
-
-    @Test
-    public void deveInteragirComAlertSimple() {
-
-        driver.findElement(By.id("alert")).click();
-        Alert alert = driver.switchTo().alert();
-        String texto = alert.getText();
-        Assert.assertEquals("Alert Simples", texto);
-        alert.accept();
-
-        driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
-
-    }
-
-    //Alert com confirm
-    @Test
-    public void deveInteragirComAlertConfirm() {
-
-        driver.findElement(By.id("confirm")).click();
-        Alert alert = driver.switchTo().alert();
-        String texto = alert.getText();
-        Assert.assertEquals("Confirm Simples",alert.getText());
-        alert.accept();
-
-    }
-
-    // interagir com prompt
-    @Test
-    public void deveInteragirComPrompt() {
-
-
-        driver.findElement(By.id("prompt")).click();
-        Alert alerta = driver.switchTo().alert();
-        Assert.assertEquals("Digite um numero", alerta.getText());
-        alerta.sendKeys("12");
-        alerta.accept();
-        Assert.assertEquals("Era 12?", alerta.getText());
-        alerta.accept();
-        Assert.assertEquals(":D", alerta.getText());
-        alerta.accept();
-
-    }
+	@Test
+	public void deveInteragirComAlertSimples(){
+		dsl.clicarBotao("alert");
+		String texto = dsl.alertaObterTextoEAceita(); 
+		Assert.assertEquals("Alert Simples", texto);
+		
+		dsl.escrever("elementosForm:nome", texto);
+	}
+	
+	@Test
+	public void deveInteragirComAlertConfirm(){
+		dsl.clicarBotao("confirm");
+		Assert.assertEquals("Confirm Simples", dsl.alertaObterTextoEAceita());
+		Assert.assertEquals("Confirmado", dsl.alertaObterTextoEAceita());
+		
+		dsl.clicarBotao("confirm");
+		Assert.assertEquals("Confirm Simples", dsl.alertaObterTextoENega());
+		Assert.assertEquals("Negado", dsl.alertaObterTextoENega());
+	}
+	
+	@Test
+	public void deveInteragirComAlertPrompt(){
+		dsl.clicarBotao("prompt");
+		Assert.assertEquals("Digite um numero", dsl.alertaObterTexto());
+		dsl.alertaEscrever("12");
+		Assert.assertEquals("Era 12?", dsl.alertaObterTextoEAceita());
+		Assert.assertEquals(":D", dsl.alertaObterTextoEAceita());
+	}
 }
--- Organização de código/teste 020 DSL/src/main/java/TesteCampoTreinamento.java
+++ Organização de código/teste 020 DSL/src/main/java/TesteCampoTreinamento.java
@@ -1,138 +1,116 @@
+import java.util.Arrays;
+import java.util.List;
 
-import com.sun.jna.platform.win32.WinNT;
-import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.List;
-
 
 public class TesteCampoTreinamento {
+	
+	private WebDriver driver;
+	private DSL dsl;
 
-    // criação de metodos para agilisar e reduzir os códigos de teste.
-    private WebDriver driver;
-    private  DSL dsl;
+	@Before
+	public void inicializa(){
+		driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		dsl = new DSL(driver);
+	}
+	
+	@After
+	public void finaliza(){
+		driver.quit();
+	}
+	
+	@Test
+	public void testeTextField(){
+		dsl.escrever("elementosForm:nome", "Teste de escrita");
+		Assert.assertEquals("Teste de escrita", dsl.obterValorCampo("elementosForm:nome"));
+	}
 
-    @Before
-    public void inicializa() {
-        driver = new FirefoxDriver();
-        driver.manage().window().setSize(new Dimension(1200, 765));
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-        dsl = new DSL(driver);
-    }
+	@Test
+	public void testTextFieldDuplo(){
+		 dsl.escrever("elementosForm:nome", "Wagner");
+		 Assert.assertEquals("Wagner", dsl.obterValorCampo("elementosForm:nome"));
+		 dsl.escrever("elementosForm:nome" , "Aquino");
+		 Assert.assertEquals("Aquino", dsl.obterValorCampo("elementosForm:nome"));
+	}
 
+	
+	@Test
+	public void deveIntegarirComTextArea(){
+		dsl.escrever("elementosForm:sugestoes", "teste\n\naasldjdlks\nUltima linha");
+		Assert.assertEquals("teste\n\naasldjdlks\nUltima linha", dsl.obterValorCampo("elementosForm:sugestoes"));
+	}
+	
+	@Test
+	public void deveIntegarirComRadioButton(){
+		dsl.clicarRadio("elementosForm:sexo:0");
+		Assert.assertTrue(dsl.isRadioMarcado("elementosForm:sexo:0"));
+	}
+	
+	@Test
+	public void deveIntegarirComCheckbox(){
+		dsl.clicarCheck("elementosForm:comidaFavorita:2");
+		Assert.assertTrue(dsl.isCheckMarcado("elementosForm:comidaFavorita:2"));
+	}
+	
+	@Test
+	public void deveIntegarirComCombo(){
+		dsl.selecionarCombo("elementosForm:escolaridade", "2o grau completo");
+		Assert.assertEquals("2o grau completo", dsl.obterValorCombo("elementosForm:escolaridade"));
+	}
+	
+	@Test
+	public void deveVerificarValoresCombo(){
+		Assert.assertEquals(8, dsl.obterQuantidadeOpcoesCombo("elementosForm:escolaridade"));
+		Assert.assertTrue(dsl.verificarOpcaoCombo("elementosForm:escolaridade", "Mestrado"));
+	}
+	
+	@Test
+	public void deveVerificarValoresComboMultiplo(){
+		dsl.selecionarCombo("elementosForm:esportes", "Natacao");
+		dsl.selecionarCombo("elementosForm:esportes", "Corrida");
+		dsl.selecionarCombo("elementosForm:esportes", "O que eh esporte?");
 
-    @After
-    public  void ficnalizar(){
-        driver.quit();
-    }
-
-    // inicialização da suite de testes.
-
-    @Test
-    public void testeTestField(){
-        dsl.escreve("elementosForm:nome","Cedric" );
-        Assert.assertEquals("Cedric", dsl.obterValorCampo("elementosForm:nome"));
-    }
-
-    @Test
-
-    public void deveInteragirComTextArea(){
-        dsl.escreve("elementosForm:sugestoes", "Teste");
-        Assert.assertEquals("Teste", dsl.obterValorCampo("elementosForm:sugestoes"));
-    }
-
-
-
-    @Test
-    public void deveInteragirComCombo() {
-        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
-        Select combo = new Select(element);
- //       combo.selectByIndex(2);
- //         combo.selectByValue("especializacao");
-        combo.selectByVisibleText("Mestrado");
-
-        Assert.assertEquals("Mestrado", combo.getFirstSelectedOption().getText());
-
-    }
-
-    @Test
-
-    public void deveInteragirValoresComCombo() {
-
-        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
-        Select combo = new Select(element);
-        List<WebElement> options = combo.getOptions();
-        Assert.assertEquals(8, options.size());
-
-        boolean  encontrou = false;
-        for (WebElement option: options){
-            if(option.getText().equals("Mestrado")){
-                encontrou = true;
-                break;
-            }
-        }
-        Assert.assertTrue(encontrou);
-    }
-
-    @Test
-
-    public void deveInteragirValoresComComboMultiplo() {
-
-        WebElement element = driver.findElement(By.id("elementosForm:esportes"));
-        Select combo = new Select(element);
-        combo.selectByVisibleText("Natacao");
-        combo.selectByVisibleText("Corrida");
-        combo.selectByVisibleText("O que eh esporte?");
-
-        List<WebElement> allSelectedOptions = combo.getAllSelectedOptions();
-        Assert.assertEquals(3,allSelectedOptions.size());
-
-        combo.deselectByVisibleText("C orrida");
-        allSelectedOptions = combo.getAllSelectedOptions();
-        Assert.assertEquals(2,allSelectedOptions.size());
-
-    }
-
-    @Test
-
-    public void deveInteragirValoresComBotao() {
-
-        WebElement botao =  driver.findElement(By.id("buttonSimple"));
-        botao.click();
-        Assert.assertEquals("Obrigado!", botao.getAttribute("value"));
-
-    }
-
-// Interargir com link  verificar se o link é clicável e o texto que é apresentado depois o click está correto
-    @Test
-    public void deveInteragirValoresComLink() {
-
-        driver.findElement(By.linkText("Voltar")).click();
-        Assert.assertEquals("Voltou!",driver.findElement(By.id("resultado")).getText());
-
-    }
-
-
-// Div e SPAN identificar textos
-    @Test
-    public void deveBuscarTextosNaPagina() {
-
-
-  //     Assert.assertTrue(driver.findElement(By.tagName("body"))
-  //              .getText().contains("Campo de Treinamento"));
-
-        Assert.assertEquals("Campo de Treinamento",
-                driver.findElement(By.tagName("h3")).getText());
-
-        Assert.assertEquals("Cuidado onde clica, muitas armadilhas...",
-                driver.findElement(By.className("facilAchar")).getText());
-
-    }
+		List<String> opcoesMarcadas = dsl.obterValoresCombo("elementosForm:esportes");
+		Assert.assertEquals(3, opcoesMarcadas.size());
+		
+		dsl.deselecionarCombo("elementosForm:esportes", "Corrida");
+		opcoesMarcadas = dsl.obterValoresCombo("elementosForm:esportes");
+		Assert.assertEquals(2, opcoesMarcadas.size());
+		Assert.assertTrue(opcoesMarcadas.containsAll(Arrays.asList("Natacao", "O que eh esporte?")));
+	}
+	
+	@Test
+	public void deveinteragirComBotoes(){
+		dsl.clicarBotao("buttonSimple");
+		Assert.assertEquals("Obrigado!", dsl.obterValueElemento("buttonSimple"));
+	}
+	
+	@Test
+	public void deveinteragirComLinks(){
+		dsl.clicarLink("Voltar");
+		
+		Assert.assertEquals("Voltou!", dsl.obterTexto("resultado"));
+	}
+	
+	@Test
+	public void deveBuscarTextosNaPagina(){
+//		Assert.assertTrue(driver.findElement(By.tagName("body"))
+//				.getText().contains("Campo de Treinamento"));
+		Assert.assertEquals("Campo de Treinamento", dsl.obterTexto(By.tagName("h3")));
+		
+		Assert.assertEquals("Cuidado onde clica, muitas armadilhas...", 
+				dsl.obterTexto(By.className("facilAchar")));
+	}
+	
 }
-// fim escopo de Elementos básicos
+
+
